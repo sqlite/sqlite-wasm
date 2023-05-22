c2f2ab3ea453e1f8da725af8da9cba5fea78c330
Thomas Steiner <tomac@google.com>
2023-05-22 12:04:18 +0200

    New release

---
+++ demo/node.js
@@ -0,0 +1,49 @@
+import sqlite3InitModule from '../index.mjs';
+
+const log = (...args) => console.log(...args);
+const error = (...args) => console.error(...args);
+
+const start = function (sqlite3) {
+  log('Running SQLite3 version', sqlite3.version.libVersion);
+  let db;
+  if ('opfs' in sqlite3) {
+    db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
+    log('OPFS is available, created persisted database at', db.filename);
+  } else {
+    db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
+    log('OPFS is not available, created transient database', db.filename);
+  }
+  try {
+    log('Creating a table...');
+    db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
+    log('Insert some data using exec()...');
+    for (let i = 20; i <= 25; ++i) {
+      db.exec({
+        sql: 'INSERT INTO t(a,b) VALUES (?,?)',
+        bind: [i, i * 2],
+      });
+    }
+    log('Query data with exec()...');
+    db.exec({
+      sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
+      callback: (row) => {
+        log(row);
+      },
+    });
+  } finally {
+    db.close();
+  }
+};
+
+log('Loading and initializing SQLite3 module...');
+sqlite3InitModule({
+  print: log,
+  printErr: error,
+}).then((sqlite3) => {
+  log('Done initializing. Running demo...');
+  try {
+    start(sqlite3);
+  } catch (err) {
+    error(err.name, err.message);
+  }
+});
