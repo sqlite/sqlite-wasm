a22372df0f02d30f0ce011c9407c10563f21f869
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2023-08-30 23:43:09 +0200

    Add SqliteClient class to type definitions

--- index.d.ts
+++ index.d.ts
@@ -8030,3 +8030,17 @@ declare type CAPI = {
   SQLITE_FAIL: 3;
   SQLITE_REPLACE: 5;
 };
+
+export class SqliteClient {
+  dbFile: string;
+  sqliteWorkerPath: string;
+
+  constructor(dFile: string, sqliteWorkerPath: string);
+
+  init(): Promise<void>;
+
+  executeSql(
+    sqlStatement: string,
+    bindParameters: SqlValue[],
+  ): Promise<SqlValue[][]>;
+}
