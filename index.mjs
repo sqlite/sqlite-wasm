31fcc755b833cb60d40c4aef69e9607ae12ea3d7
Thomas Steiner <tomac@google.com>
2023-11-10 10:25:49 +0100

    Create separate Node entry point

--- index.mjs
+++ index.mjs
@@ -1,5 +1,3 @@
 import { default as sqlite3InitModule } from './sqlite-wasm/jswasm/sqlite3-bundler-friendly.mjs';
-import { default as sqlite3InitModuleNode } from './sqlite-wasm/jswasm/sqlite3-node.mjs';
 
 export default sqlite3InitModule;
-export { sqlite3InitModuleNode };
---
+++ node.mjs
@@ -0,0 +1,3 @@
+import { default as sqlite3InitModuleNode } from './sqlite-wasm/jswasm/sqlite3-node.mjs';
+
+export { sqlite3InitModuleNode };
