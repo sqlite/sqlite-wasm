0aa44411370e1a77c2e493e95e34f25c38e97b9d
Jure Rotar <jrotar0@gmail.com>
2026-04-15 13:01:36 +0200

    chore: added worker1Promiser deprecation notices (#161)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+/** @deprecated Sqlite3Worker1Promiser is deprecated as of 2026-04-15. */
 export { default as sqlite3Worker1Promiser } from './bin/sqlite3-worker1-promiser.mjs';
 export { default } from './bin/sqlite3-bundler-friendly.mjs';
